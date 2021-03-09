4d4211dc86067152c5122b0f70c9dc59c1aa774f
Rosie Morissette <rosie.morissette@polymtl.ca>
2021-03-09 14:01:55 -0500

    ex lambda

--- exercice.py
+++ exercice.py
@@ -5,22 +5,29 @@
 
 import math
 from turtle import *
+import sys # librairire du système; path: liste qui contient tous les paths du système, recherche dans tous ces chemins
+sys.path.insert(0, "C:\\Users\morir\Documents\Polytechnique\Hiver 2021\INF1007_Programmation\Exercices GitHub\\2021h-ch6-1-exercices-mori-rosi")
+from _exercice_version_prof_ch6 import frequence
 
 # TODO: Définissez vos fonction ici
 
 #2 Volume et masse d'un ellipsoide
 
-def ellipsoide(a = 2, b = 3, c = 5, m = 50):
-    V= (4/3) * math.pi * a * b * c
+def ellipsoide(a = 2, b = 3, c = 5, m_v = 50):
+    V = (4/3) * math.pi * a * b * c
+    m = V * m_v
+    return V, m
 
-    return print(f"Le volume est {V}\n" f"La masse est {m}")
+# #3
+# # ? Pourquoi meilleur_étudiant c'est en jaune, mais le code fonctionne?
+# def meilleur_étudiant(dict_notes): # {nom_étudiant: note}
+#     x = lambda dict_notes:
+#     print(x(frequence(dict_notes)))
+#     sorted_notes = sorted(dict_notes, key=dict_notes.__getitem__, reverse=True) # renvoie une liste des noms des étudiants ordonnée des notes les plus hautes aux plus faibles
+#     return sorted_notes[0]
+#     # utiliser la fonction lambda: lambda X: for notes in dict_notes.values(): ??
 
-#3
-# ? Pourquoi meilleur_étudiant c'est en jaune, mais le code fonctionne?
-def meilleur_étudiant(dict_notes): # {nom_étudiant: note}
-    sorted_notes = sorted(dict_notes, key=dict_notes.__getitem__, reverse=True) # renvoie une liste des noms des étudiants ordonnée des notes les plus hautes aux plus faibles
-    return sorted_notes[0]
-    # utiliser la fonction lambda: lambda X: for notes in dict_notes.values(): ??
+
 
 # #4 Récursivité; dessiner un arbre
 # # avancer longueur
@@ -67,21 +74,31 @@ def proportion(chaine, sequence):
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
+
+    mass_vol = ellipsoide() #** retester
+    print(mass_vol)
+
+    volume, mass = ellipsoide()
+    print(volume)
+    print(mass)
+
     ellipsoide()
-    ellipsoide(5, m=150)
+    ellipsoide(5, m_v=150)
     ellipsoide(1, 6, 15, 14)
     ellipsoide(18, 17)
 
-    dict_1 = {"Émilie": 80, "Charles": 90}
-    print(f"Le meilleur étudiant est {meilleur_étudiant(dict_1)}.")
+    print((lambda phrase: sorted(frequence(phrase), key=frequence(phrase).__getitem__)[-1]("bonjour je m'appelle Rosie."))
 
-    chaine = "aldjsfnsrigjb"
-    sequence = "ald"
-    print(valide(chaine, sequence))
-    # print(valide("aldjsfnsrigjb", "ald"))
-    print(proportion(chaine, sequence))
-
-    chaine2 = "atgttcaacgtat"
-    sequence2 = "gt"
-    print(valide(chaine2, sequence2))
-    print(proportion(chaine2, sequence2))
+    # dict_1 = {"Émilie": 80, "Charles": 90}
+    # print(f"Le meilleur étudiant est {meilleur_étudiant(dict_1)}.")
+    #
+    # chaine = "aldjsfnsrigjb"
+    # sequence = "ald"
+    # print(valide(chaine, sequence))
+    # # print(valide("aldjsfnsrigjb", "ald"))
+    # print(proportion(chaine, sequence))
+    #
+    # chaine2 = "atgttcaacgtat"
+    # sequence2 = "gt"
+    # print(valide(chaine2, sequence2))
+    # print(proportion(chaine2, sequence2))
