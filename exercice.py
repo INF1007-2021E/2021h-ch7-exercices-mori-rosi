5b91c2e1d50ed6445b74ed319dd74ff6a15d07be
Rosie Morissette <rosie.morissette@polymtl.ca>
2021-04-20 16:37:12 -0400

    complété

--- exercice.py
+++ exercice.py
@@ -4,12 +4,12 @@
 # TODO: Importez vos modules ici
 
 import math
-
 from turtle import *
 
 import sys # librairire du système; path: liste qui contient tous les paths du système, recherche dans tous ces chemins
 sys.path.insert(0, "C:\\Users\morir\Documents\Polytechnique\Hiver 2021\INF1007_Programmation\Exercices GitHub\\2021h-ch6-1-exercices-mori-rosi")
 from _exercice_version_prof_ch6 import frequence
+
 
 # TODO: Définissez vos fonction ici
 
@@ -20,134 +20,115 @@ def ellipsoide(a = 2, b = 3, c = 5, m_v = 50):
     m = V * m_v
     return V, m
 
-#2 Ne pas définir une fonction, seulement print(lambda --> voir main)
-# ? Pourquoi meilleur_étudiant c'est en jaune, mais le code fonctionne?
-def meilleur_étudiant(dict_notes): # {nom_étudiant: note}
-    x = lambda dict_notes:
-    print(x(frequence(dict_notes)))
-    sorted_notes = sorted(dict_notes, key=dict_notes.__getitem__, reverse=True) # renvoie une liste des noms des étudiants ordonnée des notes les plus hautes aux plus faibles
-    return sorted_notes[0]
-    # utiliser la fonction lambda: lambda X: for notes in dict_notes.values(): ??
+
+#2 Ne pas définir une fonction, utiliser une fonction lambda -> seulement print(lambda...) --> voir main
 
 
+# 3 Récursivité; dessiner un arbre
 
-#3 Récursivité; dessiner un arbre
-avancer longueur
-tourner à droite 45/2 °, tourner à gauche 45/2 °
-avancer 3/4 * longueur
-reculer et faire les branches à gauche
+# avancer longueur
+# tourner à droite 45/2 °
+# avancer 3/4 * longueur
+# reculer
+# tourne angle * 2
+# avance
 
-couleur = vert
-forward
-right angle droite
-backward reculer
-pensize épaisseur des traits
+# couleur = vert
+# forward
+# right angle droite
+# backward reculer
+# pensize épaisseur des traits
 
-def draw_branch(branch_len, pen_size, angle): #--> récursive, else: rien faire donc else absent
-    if branch_len > 0 and pen_size > 0:
+def draw_branch(branch_len, pen_size, angle): # voir le pattern qui se répète (fonction récursive)
+    if branch_len > 0 and pen_size > 0: # on arrête d'appeler la fct si 1 des 3 attributs est inexistant (==0).
+                                        # on diminue les 3 arguments à mesure qu'on avance dans la récursivité
         pensize(pen_size)
         forward(branch_len)
         right(angle)
         draw_branch(branch_len - 10, pen_size - 1, angle - 5)
         left(angle * 2)
-
+        draw_branch(branch_len - 10, pen_size - 1, angle - 5)
         right(angle)
         backward(branch_len)
 
-    if  x<7   :
-        begin_fill()
-        forward(x)
-        right(45/2)
 
-    end_fill()
-    return draw_branch(0.75*x)
+    # angle change aussi
+    # critère d'arrêt -> si la condition n'est pas respectée
+    # cas de base -> ne rien faire, ne pas dessiner (n'apparaît pas dans le code)
 
-def draw_tree(): # appelle draw tree --> couleur
-    setheading(90) # faire pointer vers le haut
+def draw_tree():
+    setheading(90) # faire pointer vers le haut, pointe vers la droite par défaut
     color("green")
     draw_branch(70, 7, 35)
 
 
-# #Questions sur une fonction récursive
-#     #5 appels
-#     # 1 f(3, 5) = 3 + 12 = 15
-#     # 2 Cas de base: if b == 1: return a
-#     # 3 b va finir par être égal à 1, car il est toujours plus grand que 1 et est un entier
-#     # 4 Retourne le produit des 2 nombres
-
-
 #4 chaîne et séquence d'ADN
-
-    # ?expliquer la saisie: générer des caractères aléatoires? longueur aléatoire? ou sasir une portion d'ADN dans une séquence déjà existante? séquence <= ou < chaine
-
 def valide(chaine, sequence):
     chaine_valide = True
     sequence_valide = True
     for char in chaine:
-        if char != "a" and char != "t" and char != "g" and char != "c":
+        if (char != "a" and char != "t" and char != "g" and char != "c") or len(chaine) == 0:
             chaine_valide = False
     for char in sequence:
-        if char != "a" and char != "t" and char != "g" and char != "c":
+        if (char != "a" and char != "t" and char != "g" and char != "c") or len(sequence) == 0:
             sequence_valide = False
     if chaine_valide and sequence_valide:
         return True
     return False
 
+
 def saisie():
-    chaine = input("Entrez une chaine d'ADN:\n")
-    sequence = input("Entrez une sequence d'ADN:\n")
+    chain = input("Entrez une chaine d'ADN valide:")
+    sequence = input("Entrez une sequence d'ADN valide:")
+    if valide(chain, sequence):
+        if sequence in chain:
+            return f"chaîne: {chain}\nséquence: {sequence}\nIl y a {proportion(chain, sequence)}% de '{sequence}'."
+        return "La sequence n'est pas incluse dans la chaine."
+    return "Votre chaine ou votre sequence n'est pas valide."
 
-    return chaine, sequence
 
-# ? proportion = nb occurences (2) / nb de caract (19) * 100 ? -->
-
-#? pourquoi proportion est en jaune, mais le code fonctionne quand même?
 def proportion(chaine, sequence):
 
-    if valide(chaine, sequence):
-        if sequence in chaine:
-            occurences = chaine.count(sequence)
-            prop = occurences / len(chaine) * 100
-        return "La sequence n'est pas incluse dans la chaine."
+    if sequence in chaine:
+        occurences = chaine.count(sequence)
+        prop = occurences / len(chaine) * 100
+        return round(prop, 2)
+    return "Erreur"
 
-    else:
-        return "La chaine ou la sequence n'est pas valide."
 
-    return prop
+#5: Questions sur une fonction récursive
+    #5 appels
+    # 1 f(3, 5) = 3 + 12 = 15
+    # 2 Cas de base: if b == 1: return a
+    # 3 b va finir par être égal à 1, car il est toujours plus grand que 1 et est un entier
+    # 4 Retourne le produit des 2 nombres
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
-
-#1
-    mass_vol = ellipsoide() #** retester
-    print(mass_vol)
 
     volume, mass = ellipsoide()
     print(volume)
     print(mass)
 
-    ellipsoide()
-    ellipsoide(5, m_v=150)
-    ellipsoide(1, 6, 15, 14)
-    ellipsoide(18, 17)
+    print(ellipsoide())
+    print(ellipsoide(5, m_v=150))
+    print(ellipsoide(1, 6, 15, 14))
+    print(ellipsoide(18, 17))
 
-#2
-    print((lambda phrase: sorted(frequence(phrase), key=frequence(phrase).__getitem__)[-1])("bonjour je m'appelle Rosie."))
+    # TODO: Pas utile (#2)
+    # ma_sentence = "bonjour, je suis une phrase. je suis compose de beaucoup de lettre. oui oui"
+    # x = lambda sentence: list(frequence(ma_sentence).keys())[0]
+    # print(x)
 
-    dict_1 = {"Émilie": 80, "Charles": 90}
-    print(f"Le meilleur étudiant est {meilleur_étudiant(dict_1)}.")
+    # TODO: Solution (#2)
+    ma_sentence = "bonjour, je suis une phrase. je suis compose de beaucoup de lettre. oui oui"
+    print((lambda sentence: sorted(frequence(sentence), key=frequence(sentence).__getitem__)[-2])(ma_sentence))
+    # on peut aussi mettre reverse=True dans le sorted et prendre [0]
 
-# 3
-    draw_tree()
+    # draw_tree()
 
-#4
-    chaine = "aldjsfnsrigjb"
-    sequence = "ald"
-    print(valide(chaine, sequence))
-    # print(valide("aldjsfnsrigjb", "ald"))
-    print(proportion(chaine, sequence))
+    ma_chaine = "gctatcttctcga"
+    ma_sequence = "agc"
+    #print(saisie())
+    print(proportion(ma_chaine, ma_sequence))
 
-    chaine2 = "atgttcaacgtat"
-    sequence2 = "gt"
-    print(valide(chaine2, sequence2))
-    print(proportion(chaine2, sequence2))
